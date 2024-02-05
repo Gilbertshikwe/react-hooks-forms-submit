c6c630ab6336f28c488cf8313819333f615719aa
Gilbert <shikweg@gmail.com>
2024-02-05 09:25:33 +0300

    Completed assignment

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from "react";
+import Form from "./Form";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Form Handling in React</h2>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
